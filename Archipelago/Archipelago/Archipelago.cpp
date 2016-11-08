c4ebf84ad78283ed03d8e51d8629a8a4809b7ab0
Luther <lucson312@gmail.com>
2016-11-08 18:26:56 -0500

    Moved windows hints to header for clarity

--- Archipelago/Archipelago/Archipelago.cpp
+++ Archipelago/Archipelago/Archipelago.cpp
@@ -82,18 +82,6 @@ int main(void) {
 //////////////////////////////////////////////////////////////////////////
 
 /**
- * Initial GLFW window hints
- */
-void initGLFWHints() {
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-}
-
-/**
  * Get a GLFW window instance
  */
 GLFWwindow* getWindowInstance() {
--- Archipelago/Archipelago/Archipelago.h
+++ Archipelago/Archipelago/Archipelago.h
@@ -1,7 +1,17 @@
 #pragma once
 
 //OpenGL
-void initGLFWHints();
+/**
+* Initial GLFW window hints
+*/
+void initGLFWHints() {
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+};
 
 // Window
 GLFWwindow* getWindowInstance();
